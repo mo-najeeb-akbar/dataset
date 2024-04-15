339ee73356955a92e5a847f36737cf651f3a7196
mo-najeeb-akbar <mo.najeeb.akbar@gmail.com>
2024-04-15 10:01:56 -0700

    Initial module structure.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dataset',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    author='momo',
+    author_email='mo.najeeb.akbar@gmail.com',
+    description='Create, write, read any dataset.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mo-najeeb-akbar/dataset.git',
+    license='None',
+    classifiers=[
+
+    ]
+)
