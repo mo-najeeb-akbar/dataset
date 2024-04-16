29a7da8329e6298d111a42dd0741747ca59c2b2f
mo-najeeb-akbar <mo.najeeb.akbar@gmail.com>
2024-04-16 08:45:56 -0700

    New way to parallelize.

--- dataset/writer.py
+++ dataset/writer.py
@@ -1,10 +1,37 @@
 from .types import Datum
 from .utility import split_list, serialize
-from joblib import Parallel, delayed
 import tensorflow as tf
 import numpy as np
 import os
 import json
+import multiprocessing
+import threading
+import queue
+
+
+class TFRecordWriterPar:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.q = queue.Queue()
+        self.writer_thread = threading.Thread(target=self.write_to_tfrecord)
+        self.writer_thread.start()
+
+    def write_to_tfrecord(self):
+        with tf.io.TFRecordWriter(self.file_path) as writer:
+            while True:
+                data = self.q.get()
+                if data is None:  # None is used as a signal to stop.
+                    self.q.task_done()
+                    break
+                writer.write(data)
+                self.q.task_done()
+
+    def add_data(self, processed_data):
+        self.q.put(processed_data)
+
+    def close(self):
+        self.q.put(None)
+        self.writer_thread.join()
 
 
 def write_dataset(
@@ -12,9 +39,7 @@ def write_dataset(
         data_refs: list[Datum],
         output_path: str,
         extra_identifiers: list[str] | None = None,
-        num_shards: int = 1,
-        num_workers: int = 1,
-        verbose: int = 0
+        num_shards: int = 1
 ) -> None:
     """
 
@@ -23,32 +48,32 @@ def write_dataset(
     :param output_path: location of folder where to write data
     :param extra_identifiers: list of strings appended to file names for more information
     :param num_shards: number of separate chunks to write data as
-    :param num_workers: number of processes to spin up
-    :param verbose: {0: silent, 1: chunk completion, 2: datum completion}
     :return:
 
     """
-    sharded_data_refs = split_list(data_refs, num_shards)
     extra_suffix = '' if extra_identifiers is None else '_' + '_'.join(extra_identifiers)
     output_file_pre = os.path.join(output_path, f'record{extra_suffix}_')
 
-    def process_chunk(data: list[list[Datum]], id_: int):
-        writer_tf = tf.io.TFRecordWriter(f'{output_file_pre}{id_}.tfrecord')
-        for idx, dat in enumerate(data):
-            serializable_units = decode_func(dat)
-            if serializable_units is not None:
-                serial_dict = serialize(serializable_units)
-                example_proto = tf.train.Example(features=tf.train.Features(feature=serial_dict))
-                writer_tf.write(example_proto.SerializeToString())
-            if verbose == 2:
-                print(f'Processed datum number: {idx} in chunk {id_}.')
-        if verbose == 1:
-            print(f'Finished writing chunk number: {id_}.')
-        writer_tf.close()
-    Parallel(n_jobs=num_workers)(
-        delayed(process_chunk)(references, shard_id)
-            for shard_id, references in enumerate(sharded_data_refs)
-    )
+    def worker(data: list[Datum], writer: TFRecordWriterPar):
+        serializable_units = decode_func(data)
+        if serializable_units is not None:
+            serial_dict = serialize(serializable_units)
+            example_proto = tf.train.Example(features=tf.train.Features(feature=serial_dict))
+            writer.add_data(example_proto.SerializeToString())
+
+    writers = [TFRecordWriterPar(output_file_pre + f'{num}.tfrecord') for num in range(num_shards)]
+
+    # Setup multiprocessing pool
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        # map_async for non-blocking calls
+        [pool.apply_async(worker, args=(data, writers[idx // num_shards])) for idx, data in enumerate(data_refs)]
+        # Close pool and wait for work to finish
+        pool.close()
+        pool.join()
+
+    for w in writers:
+        w.close()
+
 
 
 def write_parser_dict(
